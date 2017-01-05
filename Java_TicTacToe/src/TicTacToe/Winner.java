42572055169cd2800ca02f2a73298841622878b0
MuJavaKtw <mu.javaktw@gmail.com>
2017-01-05 22:10:55 +0100

    D
    D

--- Java_TicTacToe/src/TicTacToe/Winner.java
+++ Java_TicTacToe/src/TicTacToe/Winner.java
@@ -32,7 +32,6 @@ public class Winner {
             col = (board[i][0] == (board[i][1]) && board[i][0] == (board[i][2])) && board[i][0] != 0;
             diagonal1 = (board[0][0] == (board[1][1]) && board[0][0] == (board[2][2])) && board[1][1] != 0;
             diagonal2 = (board[0][2] == (board[1][1]) && board[0][2] == (board[2][0])) && board[1][1] != 0;
-            //   (board[0][2] == (board[1][1]) && board[1][1] == (board[2][0]));
             if (col) {
                 winner(board[i][0]);
                 return false;
